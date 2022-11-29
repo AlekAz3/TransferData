f82a2f125cdaf850b7ef9f6a97521db16dc19d0a
vakarin2020@gmail.com <vakarin2020@gmail.com>
2022-11-29 12:07:35 +0300

    Первая готовая бизнес логика + тесты

---
+++ TransferData.Model/IDataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TransferData.Model
+{
+    public interface IDataContext
+    {
+        DbSet<InformationSchema> schema { get; }
+        DbType type { get; }
+    }
+}
---
+++ TransferData.Model/InformationSchema.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TransferData.Model
+{
+    [Table("INFORMATION_SCHEMA.COLUMNS")]
+    public class InformationSchema
+    {
+        [Column("TABLE_SCHEMA")]
+        public string table_schema { get; set; }
+
+        [Column("TABLE_NAME")]
+        public string table_name { get; set; }
+
+        [Column("COLUMN_NAME")]
+        public string column_name { get; set; }
+
+        [Column("DATA_TYPE")]
+        public string data_type { get; set; }
+
+    }
+}
