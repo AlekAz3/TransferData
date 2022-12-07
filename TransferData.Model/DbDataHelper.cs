27223622d5898cfd9adbf01a01df3aa962d4e302
vakarin2020@gmail.com <vakarin2020@gmail.com>
2022-12-07 15:31:57 +0300

    список типов данных для которых НЕ нужны апострофы

--- TransferData.Model/DbDataHelper.cs
+++ TransferData.Model/DbDataHelper.cs
@@ -30,33 +30,64 @@
         {
             List<List<string>> quetes = new List<List<string>>()
             {
+                //new List<string>() //PostgreSQL
+                //{
+                //    "character varying",
+                //    "varchar",
+                //    "char",
+                //    "character",
+                //    "text",
+                //    "timestamp",
+                //    "date",
+                //    "time",
+                //    "interval"
+                //},
+                //new List<string>() //MSSQL
+                //{
+                //    "varchar",
+                //    "date",
+                //    "datetime",
+                //    "datetime2",
+                //    "datetimeoffset",
+                //    "smalldatetime",
+                //    "time",
+                //    "timestamp",
+                //    "char",
+                //    "varchar",
+                //    "text",
+                //    "nvarchar",
+                //    "nchar",
+                //    "ntext",
+                //}
+
                 new List<string>() //PostgreSQL
                 {
-                    "character varying",
-                    "varchar",
-                    "char",
-                    "character",
-                    "text",
-                    "timestamp",
-                    "date",
-                    "time",
-                    "interval"
+                    "smallint",
+                    "integer",
+                    "bigint",
+                    "decimal",
+                    "numeric",
+                    "real",
+                    "double precision",
+                    "bytea"
+
                 },
                 new List<string>() //MSSQL
                 {
-                    "varchar",
-                    "date",
-                    "datetime",
-                    "datetime2",
-                    "datetimeoffset",
-                    "smalldatetime",
-                    "time",
-                    "char",
-                    "varchar",
-                    "text",
-                    "nvarchar",
-                    "nchar",
-                    "ntext",
+                    "bigint",
+                    "int",
+                    "smallint",
+                    "tinyint",
+                    "bit",
+                    "decimal",
+                    "numeric",
+                    "money",
+                    "smallmoney",
+                    "float",
+                    "real",
+                    "binary",
+                    "varbinary"
+
                 }
             };
 
@@ -64,7 +95,7 @@
             string result = String.Empty;
 
             for (int i = 0; i < input.Count; i++)
-                if (quetes[(int)dbType].Contains(columns[i].FieldType) && input[i] != "null")
+                if (!quetes[(int)dbType].Contains(columns[i].FieldType) && input[i] != "null")
                     result += $"'{input[i]}' as {columns[i].FieldName}, ";
                 else
                     result += $"{input[i]} as {columns[i].FieldName}, ";
