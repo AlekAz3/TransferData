20a97027961de7bb362b074a97b57ed624dc1972
vakarin2020@gmail.com <vakarin2020@gmail.com>
2022-12-09 13:30:08 +0300

    Тип данных дата для PostgreSQL

--- TransferData.Model/Models/SchemaInfo.cs
+++ TransferData.Model/Models/SchemaInfo.cs
@@ -25,76 +25,12 @@
 
         internal string FieldsWithQuotes(List<string> input, DbType dbType)
         {
-            List<List<string>> quetes = new List<List<string>>()
-            {
-                //new List<string>() //PostgreSQL
-                //{
-                //    "character varying",
-                //    "varchar",
-                //    "char",
-                //    "character",
-                //    "text",
-                //    "timestamp",
-                //    "date",
-                //    "time",
-                //    "interval"
-                //},
-                //new List<string>() //MSSQL
-                //{
-                //    "varchar",
-                //    "date",
-                //    "datetime",
-                //    "datetime2",
-                //    "datetimeoffset",
-                //    "smalldatetime",
-                //    "time",
-                //    "timestamp",
-                //    "char",
-                //    "varchar",
-                //    "text",
-                //    "nvarchar",
-                //    "nchar",
-                //    "ntext",
-                //}
-
-                new List<string>() //PostgreSQL
-                {
-                    "smallint",
-                    "integer",
-                    "bigint",
-                    "decimal",
-                    "numeric",
-                    "real",
-                    "double precision",
-                    "bytea"
-
-                },
-                new List<string>() //MSSQL
-                {
-                    "bigint",
-                    "int",
-                    "smallint",
-                    "tinyint",
-                    "bit",
-                    "decimal",
-                    "numeric",
-                    "money",
-                    "smallmoney",
-                    "float",
-                    "real",
-                    "binary",
-                    "varbinary"
-
-                }
-            };
-
             List<string> result = new List<string>();
 
             for (int i = 0; i < input.Count; i++)
-                if (!quetes[(int)dbType].Contains(Fields[i].FieldType) && input[i] != "null")
-                    result.Add($"'{input[i]}' as {Fields[i].FieldName}");
-                else
-                    result.Add($"{input[i]} as {Fields[i].FieldName}");
+            {
+                result.Add($"{Fields[i].dataCheckQuotes(input[i], dbType)} as {Fields[i].FieldName}");
+            }
 
             return string.Join(", ", result);
 
